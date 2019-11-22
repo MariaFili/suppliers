723f2e27ea63f309eae1abac39ff8cc2fed15b69
Maria <F_marria@bk.ru>
2019-11-22 08:51:13 +0300

    not released

--- models/categories.js
+++ models/categories.js
@@ -1,9 +1,9 @@
-// const mongoose = require("mongoose");
-// const { ObjectId } = mongoose.Schema.Types;
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema.Types;
 
-// const CategorieSchema = new mongoose.Schema({
-//   name: String,
-//   author: { type: ObjectId, ref: "User" }
-// });
+const CategorieSchema = new mongoose.Schema({
+  name: String,
+  author: { type: ObjectId, ref: "User" }
+});
 
-// module.exports = mongoose.model("Category", CategorieSchema);
+module.exports = mongoose.model("Category", CategorieSchema);
--- routes/category.js
+++ routes/category.js
@@ -1,27 +1,35 @@
 const express = require("express");
 const router = express.Router();
-// const Category = require('../models/category');
+const Advertisement = require("../models/ads");
 
 
 
-// const category = articlesModels.carsModel;
-// const estateModel = articlesModels.estateModel;
-// const petsModel = articlesModels.petsModel;
+router.get("/:id", async function(req, res) {
+  let entries = await Advertisement.findById(req.params.id);
+  
 
-/* GET home page. */
-router.get("/", function(req, res) {
-  // let currentCategory = await Category.find();
-  res.render("category");
+  res.render("category", {advertisements: entries});
 });
 
-// router.get("/estate", async function(req, res) {
-//   let currentArticle = await estateModel.find();
-//   res.render("articles", { article: currentArticle });
+
+// // const category = articlesModels.carsModel;
+// // const estateModel = articlesModels.estateModel;
+// // const petsModel = articlesModels.petsModel;
+
+// /* GET home page. */
+// router.get("/", function(req, res) {
+//   // let currentCategory = await Category.find();
+//   res.render("category");
 // });
-// router.get("/pets", async function(req, res) {
-//   let currentArticle = await petsModel.find();
-//   res.render("articles", { article: currentArticle });
-// });
+
+// // router.get("/estate", async function(req, res) {
+// //   let currentArticle = await estateModel.find();
+// //   res.render("articles", { article: currentArticle });
+// // });
+// // router.get("/pets", async function(req, res) {
+// //   let currentArticle = await petsModel.find();
+// //   res.render("articles", { article: currentArticle });
+// // });
 
 
 
